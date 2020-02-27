16fd05042bbc52bcc3c6d0d729744373d82873b6
edzaans-git <g00377012@gmit.ie>
2020-02-27 14:05:28 +0000

    second change

--- Person.java
+++ Person.java
@@ -4,6 +4,17 @@ public class Person {
     private String middleNames;
     private String userName;
 
+      //Setter Methods
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+     public void setMiddleName(String middleNames) {
+        this.middleNames = middleNames;
+    }
+     public void setLastName(String userName) {
+        this.firstName = userName;
+    }
+
     // Getter Methods
     public String getFirstName() {
         return firstName;
@@ -13,17 +24,6 @@ public class Person {
     }
      public String getUserName() {
         return userName;
-    }
-
-    //Setter Methods
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-     public void setMiddleName(String middleNames) {
-        this.middleNames = middleNames;
-    }
-     public void setLastName(String userName) {
-        this.firstName = userName;
     }
 
 }
