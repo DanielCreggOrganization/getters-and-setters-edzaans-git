a763e684bbb0de80ec81f631b203eb090909c091
edzaans-git <g00377012@gmit.ie>
2020-02-27 14:10:55 +0000

    second object

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ public class Main {
        Person person = new Person();
        person.setFirstName("Freddy");
        System.out.println(person.getFirstName());
+
+         Person person2 = new Person();
+       person2.setMiddleName("Middler");
+       System.out.println(person2.getMiddleNames());
    }
 }
