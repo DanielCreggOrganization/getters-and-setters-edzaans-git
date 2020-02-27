6825451311cddb0b3c2cceb96db41bfdcc6d4336
edzaans-git <g00377012@gmit.ie>
2020-02-27 14:03:56 +0000

    Getters,setters

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    // Main method
+   public static void main (String args[]) {
+       Person person = new Person();
+       person.setFirstName("Freddy");
+       System.out.println(person.getFirstName());
+   }
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,29 @@
-class Person {
-    // Instance Variables
-    
-    // Methods
+public class Person {
+    // Instance Variables(String=firstName,)
+    private String firstName;
+    private String middleNames;
+    private String userName;
+
+    // Getter Methods
+    public String getFirstName() {
+        return firstName;
+    }
+     public String getMiddleNames() {
+        return middleNames;
+    }
+     public String getUserName() {
+        return userName;
+    }
+
+    //Setter Methods
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+     public void setMiddleName(String middleNames) {
+        this.middleNames = middleNames;
+    }
+     public void setLastName(String userName) {
+        this.firstName = userName;
+    }
+
 }
